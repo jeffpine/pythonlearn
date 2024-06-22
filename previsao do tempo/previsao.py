ac58891905c30e2a0ff6834c0d2e63ad072bc6e1
jeffpine <65078428+jeffpine@users.noreply.github.com>
2024-06-22 20:04:36 -0300

    Add files via upload

--- previsao do tempo/previsao.py
+++ previsao do tempo/previsao.py
@@ -1,7 +1,7 @@
 import requests
 
 def obter_previsao_tempo(cidade, api_key):
-    url = f"http://api.openweathermap.org/data/2.5/weather?q={cidade}&appid={api_key}&lang=pt_br&units=metric"
+    url = f"https://api.openweathermap.org/data/2.5/weather?q={cidade}&appid={api_key}&lang=pt_br"
     resposta = requests.get(url)
     if resposta.status_code == 200:
         dados = resposta.json()
